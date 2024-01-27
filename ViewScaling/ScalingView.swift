2a9247d494816375b8b24a19a75ca368e0035bf1
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2024-01-27 10:35:14 +0900

    Change timing of view settings
    Transfers settings for views that are only performed once at initialization.

--- ViewScaling/ScalingView.swift
+++ ViewScaling/ScalingView.swift
@@ -13,19 +13,25 @@ class ScalingView: UIView {
   override init(frame: CGRect) {
     subview = UIView()
     super.init(frame: frame)
+    
+    subview.backgroundColor = .white
+    let subviewSize = 100
+    subview.frame.size = CGSize(width: subviewSize, height: subviewSize)
+    subview.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+    
     addSubview(subview)
-    self.scaleSubview()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    scaleSubview()
+  }
+  
   func scaleSubview() {
-    subview.backgroundColor = .white
-    let subviewSize = 100
-    subview.frame.size = CGSize(width: subviewSize, height: subviewSize)
-    subview.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
-    subview.transform = CGAffineTransform(scaleX: 2, y: 2)
+    subview.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
   }
 }
